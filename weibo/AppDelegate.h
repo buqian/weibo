365e2ffcd8c20ab8c77159f1409ed5294dd54cc0
张富强 <3448622@qq.com>
2015-11-12 20:01:21 +0800

    Initial Commit

---
+++ weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by zhangfuqiang on 15/11/12.
+//  Copyright © 2015年 zhangfuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by zhangfuqiang on 15/11/12.
+//  Copyright © 2015年 zhangfuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
