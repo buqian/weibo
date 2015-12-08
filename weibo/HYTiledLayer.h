c0d3d762705f86a3dcd2ea1a021d52e06fba5f07
张富强 <3448622@qq.com>
2015-12-08 17:13:35 +0800

    1

---
+++ weibo/HYTiledLayer.h
@@ -0,0 +1,13 @@
+//
+//  HYTiledLayer.h
+//  weibo
+//
+//  Created by zhangfuqiang on 15/12/8.
+//  Copyright © 2015年 zhangfuqiang. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface HYTiledLayer : CATiledLayer
+
+@end
